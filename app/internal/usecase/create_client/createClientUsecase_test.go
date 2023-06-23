f2d4ed54c4134528632532cb38ab249b20b21f81
Silvio Almeida <silviotmalmeida@gmail.com>
2023-06-22 21:18:15 -0300

    Ajustes gerais

--- app/internal/usecase/create_client/createClientUsecase_test.go
+++ app/internal/usecase/create_client/createClientUsecase_test.go
@@ -17,7 +17,7 @@ type ClientGatewayMock struct {
 func (m *ClientGatewayMock) Save(client *entity.Client) error {
 	// recebe o client
 	args := m.Called(client)
-	// ???
+	// retorna como argumento 0 um erro
 	return args.Error(0)
 }
 
@@ -25,7 +25,8 @@ func (m *ClientGatewayMock) Save(client *entity.Client) error {
 func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
 	// recebe o id
 	args := m.Called(id)
-	// ???
+	// retorna como argumento 0 um client
+	// retorna como argumento 1 um erro
 	return args.Get(0).(*entity.Client), args.Error(1)
 }
 
